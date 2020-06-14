4b9476ea129f4ec12c6a4de8ada0b8ffe68782c5
VICTOR MANUEL <VICTOR MANUEL@DESKTOP-70E8UMN>
2020-06-13 20:14:15 -0500

    Adicion de GUINotificaciones y modificacion de CLSNotifiacionImpl

--- src/servidorNotificaciones/sop_rmi/ClsNotificacionImpl.java
+++ src/servidorNotificaciones/sop_rmi/ClsNotificacionImpl.java
@@ -4,51 +4,71 @@ package servidorNotificaciones.sop_rmi;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.HashMap;
 import servidorAlertas.dao.ClsAsintomaticoDAOImpl;
 import servidorAlertas.dao.AsintomaticoDAOInt;
 import servidorAlertas.dao.ClsAsintomaticoDAO;
 import servidorAlertas.dto.ClsAsintomaticoDTO;
+import servidorNotificaciones.GUINotificaciones;
 import servidorNotificaciones.dto.ClsMensajeNotificacionDTO;
 
 
 public class ClsNotificacionImpl extends UnicastRemoteObject implements NotificacionInt{
+    HashMap<ClsAsintomaticoDTO, GUINotificaciones> GUIAsintomaticos;
 
     public ClsNotificacionImpl() throws RemoteException{
+        GUIAsintomaticos = new HashMap();
     }
     
     @Override
     public void notificarRegistro(ClsMensajeNotificacionDTO objMensajeNotificacion) throws RemoteException {
         
         System.out.println("Desde notificarRegistro()...");
-        
+     
         ClsAsintomaticoDTO pacienteAsintomatico = objMensajeNotificacion.getPacienteAsintomatico();
         ArrayList<ClsAsintomaticoDAO> pacientesDAO;
         int frecuanciaCardiaca, frecuenciaRespiratoria;
         float temperatura;
         
-        System.out.println("Alerta Generada");
+        /*System.out.println("Alerta Generada");
         System.out.println("Id: "+pacienteAsintomatico.getId());
         System.out.println("Nombres: "+pacienteAsintomatico.getNombres());
         System.out.println("Apellidos: "+pacienteAsintomatico.getApellidos());
-        System.out.println("Direccion: "+pacienteAsintomatico.getDireccion());
+        System.out.println("Direccion: "+pacienteAsintomatico.getDireccion());*/
+        //Para cada paciente se crea un GUI
+        GUINotificaciones GUI;
+        if(!GUIAsintomaticos.containsKey(pacienteAsintomatico)){
+            GUI = new GUINotificaciones();
+            GUIAsintomaticos.put(pacienteAsintomatico, GUI);
+        }else
+            GUI = GUIAsintomaticos.get(pacienteAsintomatico);
+        //Enviar al GUI los datos del paciente
+        GUI.fijarAsintomatico(pacienteAsintomatico.getTipo_id(),pacienteAsintomatico.getId(),pacienteAsintomatico.getNombres(),
+        pacienteAsintomatico.getApellidos(), pacienteAsintomatico.getDireccion());
         frecuanciaCardiaca = objMensajeNotificacion.getFrecuenciaCardiaca();
         frecuenciaRespiratoria = objMensajeNotificacion.getFrecuenciaRespiratoria();
         temperatura = objMensajeNotificacion.getTemperatura();
-        System.out.println("Indicadores que generaron la Alerta:");
+        //System.out.println("Indicadores que generaron la Alerta:");
+        //Enviar al GUI los indicadores
         if(frecuanciaCardiaca != 0)
-            System.out.println("Frecuencia Cardiaca: "+frecuanciaCardiaca);
+            //System.out.println("Frecuencia Cardiaca: "+frecuanciaCardiaca);
+            GUI.fijarFrecuenciaCardiaca(frecuanciaCardiaca);
         if(frecuenciaRespiratoria != 0)
-            System.out.println("Frecuencia Respiratoria: "+frecuenciaRespiratoria);
+            //System.out.println("Frecuencia Respiratoria: "+frecuenciaRespiratoria);
+            GUI.fijarFrecuenciaRespiratoria(frecuenciaRespiratoria);
         if(temperatura != 0)
-            System.out.println("Temperatura: "+temperatura);
-        
-        System.out.println("El personal medico debe revisar el paciente");
+            //System.out.println("Temperatura: "+temperatura);
+            GUI.fijarTemperatura(temperatura);
+        //Enviar al GUI el mensaje del tipo de alerta
+        String mensaje = ("El personal medico debe revisar al paciente");
+        //System.out.println("El personal medico debe revisar el paciente");
         AsintomaticoDAOInt objetoAsintomaticoDAO = new ClsAsintomaticoDAOImpl();
         pacientesDAO = objetoAsintomaticoDAO.leerHistorialAsintomatico(pacienteAsintomatico.getId());
         
         for(ClsAsintomaticoDAO objPacienteDAO: pacientesDAO)
         {
-            System.out.println(objPacienteDAO.getFechaAlerta()+"   "+objPacienteDAO.getHoraAlerta()+"          "+objPacienteDAO.getPuntuacion());
+            GUI.fijarAlerta(objPacienteDAO.getFechaAlerta(), objPacienteDAO.getHoraAlerta(), objPacienteDAO.getPuntuacion());
+            //System.out.println(objPacienteDAO.getFechaAlerta()+"   "+objPacienteDAO.getHoraAlerta()+"          "+objPacienteDAO.getPuntuacion());
         }
         
         System.out.println("Saliendo de notificarRegistro()...");
